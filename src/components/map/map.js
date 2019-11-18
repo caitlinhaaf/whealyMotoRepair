1ce27a378b79a1045bf4a434f7307e71bc48aacc
Caitlin Haaf <caitlinhaafillustration@gmail.com>
2019-11-18 15:04:34 -0500

    Added custom Google map style data; disabled certain default map controls (map type, street view controls).

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -10,6 +10,7 @@ const GoogleMap = ({...props}) => {
                 edges {
                     node {
                     results {
+                        formatted_address,
                         geometry {
                         location {
                             lat
@@ -23,26 +24,30 @@ const GoogleMap = ({...props}) => {
         }
     `)
     const mapObj = mapData.allLocationData.edges[0].node.results[0].geometry.location
-    
+
     return(
-        <div style={{position: `relative`, width: `100%`, height: `100%`}}>
-            <Map
-            defaultOptions={{
-                styles: mapStyles,
-            }}
-            google={props.google}
-            zoom={16}
-            style={{width: `100%`, height: `100%`, minHeight: `300px`}}
-            initialCenter={{ lat: mapObj.lat, lng: mapObj.lng }}
-            >
-                <Marker position={{ lat: mapObj.lat, lng: mapObj.lng }} />
-            </Map>
-        </div>
+        <>
+            <span style={{display: `block`, marginBottom: `.5rem`}}>
+                {mapData.allLocationData.edges[0].node.results[0].formatted_address}
+            </span>
+            <div style={{position: `relative`, width: `100%`, height: `100%`}}>
+                <Map
+                google={props.google}
+                zoom={16}
+                mapTypeControl={false}
+                streetViewControl={false}
+                style={{width: `100%`, height: `100%`, minHeight: `300px`}}
+                styles={mapStyles}
+                initialCenter={{ lat: mapObj.lat, lng: mapObj.lng }}
+                >
+                    <Marker position={{ lat: mapObj.lat, lng: mapObj.lng }} />
+                </Map>
+            </div>
+        </>
         
     )
 }
 
-// export default GoogleMap
 export default GoogleApiWrapper({
     apiKey: `${process.env.GATSBY_WHEALY_MAPS_API_KEY}`
   })(GoogleMap);
